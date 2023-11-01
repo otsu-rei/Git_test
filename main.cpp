45cd2788eddf098cd350622a613ba17e72049806
sxavenger <sxavenger.x3@gmail.com>
2023-11-01 11:57:49 +0900

    pull!

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		Novice::ScreenPrintf(0, 0, "Test comment");
+		Novice::ScreenPrintf(0, 0, "pullReqest");
 		Novice::ScreenPrintf(0, 20, "//2023, 10, 18");
 
 		// working plz...
